8d3890046ec1817af9f62417084744354d6f47f8
Glenon Mateus <glenonmateus@gmail.com>
2023-08-20 09:27:48 -0300

    add operadores aritméticos

---
+++ operadores/index.js
@@ -0,0 +1,40 @@
+/*
+ * Aritméticos
+ * + Adição / Concatenação ( - / * )
+ * ** Potenciação
+ * % Resto da divisão
+ */
+
+/*
+ * Precedências
+ * ()
+ * **
+ * * / %
+ * + -
+ */
+
+/*
+ * Incremento
+ * operador ++
+ * Decremento
+ * operador --
+ */
+
+let contador = 1;
+contador++;
+contador++;
+++contador;
+console.log(contador);
+
+let contador2 = 1;
+console.log(contador2++);
+console.log(contador2);
+
+let contador3 = 1;
+console.log(++contador3);
+
+let contador4 = 0;
+contador4 += 2; // contador4 = contador4 + 2
+// funciona para qualquer operador aritmético
+// *= -= /= **=
+console.log(contador4);
