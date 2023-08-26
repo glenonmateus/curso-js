01f291245aa058a09b18639189e489261b0b5809
Glenon Mateus <glenonmateus@gmail.com>
2023-08-26 10:08:05 -0300

    add exercicio

--- tiposdedados/index.js
+++ tiposdedados/index.js
@@ -22,3 +22,25 @@ console.log(x, y);
 
 y.push(3);
 console.log(x, y);
+
+// string tem indices
+let umaString = "Um texto";
+console.log(umaString);
+console.log("Indice 0:", umaString[0]);
+console.log("Indice 9:", umaString[9]); //undefined
+console.log("Indice 9:", umaString.charAt(9)); //
+console.log("Concat:", umaString.concat(" em um lindo dia"));
+console.log("IndexOf:", umaString.indexOf("texto")); //começa do inicio da string
+console.log("lastIndexOf of:", umaString.lastIndexOf("texto")); //começa do final da string
+console.log("Match:", umaString.match(/[a-z]/g));
+console.log("Search:", umaString.search(/x/)); // similar ao indexOf, porém aceita expressões regulares
+let outraString = "o rato roeu a roupa do rei de roma.";
+console.log("Replace:", outraString.replace(/r/g, "#"));
+console.log("Length:", outraString.length);
+console.log("Slice:", outraString.slice(3, 6));
+console.log("Slice negativo:", outraString.slice(-3)); // outraString.length - 3 (32)
+console.log(outraString.slice(32));
+console.log(outraString.slice(-5));
+console.log("Split:", outraString.split(" "));
+console.log("toUpperCase:", outraString.toUpperCase());
+console.log("toLowerCase:", outraString.toLowerCase());
