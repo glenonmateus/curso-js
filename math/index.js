a27a4b97b8a1c3642a208ca37886afe7e4551959
Glenon Mateus <glenonmateus@gmail.com>
2023-08-26 12:03:12 -0300

    add math

---
+++ math/index.js
@@ -0,0 +1,12 @@
+// const num1 = 9.345236;
+// let num2 = Math.floor(num1); // arrendoda para baixo
+// let num2 = Math.ceil(num1); // arrendoda para cima
+// const num2 = Math.round(num1); // arrendoda para número mais próximo
+// const max = Math.max(1, 3, 4, 5, 6, 78, 123, -4, 9.3, 213.4);
+// const min = Math.min(1, 3, 4, 5, 6, 78, 123, -4, 9.3, 213.4);
+// const rand = Math.random(); // número aleatório, entre 0 e 1
+// const rand = Math.random() * (10 - 5) + 5; // número aleatório entre 5 e 10
+// const num1 = 9;
+// const raiz = num1 ** (1 / 2); // num1 ** 0.5
+const divisaoPorZero = 100 / 0; // Infinity (avaliado como verdadeiro)
+console.log(divisaoPorZero);
