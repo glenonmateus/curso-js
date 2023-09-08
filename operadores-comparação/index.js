b16a7d909922b17303562532028a6b1f94731881
Glenon Mateus <glenonmateus@gmail.com>
2023-09-08 10:11:19 -0300

    add operadores de comparação

---
+++ operadores-comparação/index.js
@@ -0,0 +1,16 @@
+/*
+ * Operadores de compração
+ * > maior que
+ * >= maior que ou igual a
+ * < menor que
+ * <= menor que ou igual a
+ * == igualdade
+ * === igualdade estrita (valor e tipo)
+ * != diferente
+ * !== diferente estrito (valor e tipo)
+ */
+console.log(10 > 5);
+console.log(10 >= 5);
+console.log(10 >= 11);
+console.log(10 == "10");
+console.log(10 === "10");
