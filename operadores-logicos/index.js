d96bf69822b926a01314a890e3b0bcbed4983372
Glenon Mateus <glenonmateus@gmail.com>
2023-09-08 10:22:01 -0300

    add operadores lógicos

---
+++ operadores-logicos/index.js
@@ -0,0 +1,19 @@
+/*
+ * Operadores lógicos
+ * && -> AND -> E -> todas as expressões precisam ser verdadeiras para retornar 'true'
+ * || -> OR -> OU ->
+ * ! -> NOT -> NÃO ->
+ */
+// const expressaoAnd = true && true && false && true;
+// console.log(expressaoAnd);
+//
+// const expressaoOr = false || true || false || false;
+// console.log(expressaoOr);
+// const usuario = "Glenon"; // form usuário digitou
+// const senha = "123456"; // form usuário digitou
+//
+// const vailogar = usuario === "Glenon" && senha === "123456";
+// console.log(vailogar);
+
+console.log(!true);
+console.log(!false);
