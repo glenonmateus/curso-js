a0a222b3628b9f87f55d0ac044b2d6bfb49eb0e2
Glenon Mateus <glenonmateus@gmail.com>
2023-09-29 10:21:06 -0300

    refactor if

--- exercicios/exercicio06/assets/js/main.js
+++ exercicios/exercicio06/assets/js/main.js
@@ -34,21 +34,12 @@ const getNivelImc = (imc) => {
     "Obesidade grau 2",
     "Obesidade grau 3",
   ];
-  if (imc >= 39.9) {
-    return nivel[5];
-  }
-  if (imc >= 34.9) {
-    return nivel[4];
-  }
-  if (imc >= 29.9) {
-    return nivel[3];
-  }
-  if (imc >= 18.5) {
-    return nivel[2];
-  }
-  if (imc < 18.5) {
-    return nivel[1];
-  }
+  if (imc >= 39.9) return nivel[5];
+  if (imc >= 34.9) return nivel[4];
+  if (imc >= 29.9) return nivel[3];
+  if (imc >= 24.9) return nivel[2];
+  if (imc >= 18.5) return nivel[1];
+  if (imc < 18.5) return nivel[0];
 };
 
 const setResultado = (msg, isValid) => {
