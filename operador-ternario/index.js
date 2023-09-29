e8ae3b5293406276c0692fbe4983cc3a625e3c8c
Glenon Mateus <glenonmateus@gmail.com>
2023-09-29 10:20:51 -0300

    add operador ternário

---
+++ operador-ternario/index.js
@@ -0,0 +1,14 @@
+// ? :
+// (condição) ? 'valor para verdadeiro' : 'valor para falso'
+const pontuacao = 999;
+
+// if (pontuacao > 999) {
+//   console.log("Usuário VIP");
+// } else {
+//   console.log("Usuário normal");
+// }
+
+const nivel = pontuacao > 999 ? "Usuário VIP" : "Usuário normal";
+
+const cor = null;
+const corPadrao = cor || "Preto";
