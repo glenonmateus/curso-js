3eced958a5b66c86e268531bccfe8a38259d5650
Glenon Mateus <glenonmateus@gmail.com>
2023-10-05 08:51:01 -0300

    add for

---
+++ for/index.js
@@ -0,0 +1,11 @@
+// for (let index = 0; index < 6; index++) {
+//   console.log(index);
+// }
+// for (let index = 0; index < 11; index++) {
+//   const par = index % 2 === 0 ? "par" : "ímpar";
+//   console.log(index, par);
+// }
+const frutas = ["maçã", "pêra", "uva"];
+for (let index = 0; index < frutas.length; index++) {
+  console.log(frutas[index]);
+}
