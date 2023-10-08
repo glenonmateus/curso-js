4b065a64296e5489fd5a2811763184f2cfa02a30
Glenon Mateus <glenonmateus@gmail.com>
2023-10-08 12:02:27 -0300

    add for of and foreach

--- for/index.js
+++ for/index.js
@@ -9,19 +9,41 @@
 // for (let index = 0; index < frutas.length; index++) {
 //   console.log(frutas[index]);
 // }
-const frutas = ["maçã", "pêra", "uva"];
+// const frutas = ["maçã", "pêra", "uva"];
 
 // ler os indices ou chaves do objeto
-for (let index in frutas) {
-  console.log(frutas[index]);
+// for (let index in frutas) {
+//   console.log(frutas[index]);
+// }
+//
+// const pessoa = {
+//   nome: "Glenon",
+//   sobronome: "Araujo",
+//   idade: 30,
+// };
+//
+// for (const key in pessoa) {
+//   console.log(pessoa[key]);
+// }
+
+const nomes = ["Glenon", "Mateus", "Barbosa", "Araujo"];
+
+for (const index in nomes) {
+  console.log(nomes[index]);
 }
 
-const pessoa = {
-  nome: "Glenon",
-  sobronome: "Araujo",
-  idade: 30,
-};
+console.log("\n");
 
-for (const key in pessoa) {
-  console.log(pessoa[key]);
+for (const value of nomes) {
+  console.log(value);
 }
+
+console.log("\n");
+
+nomes.forEach(function(element, index) {
+  console.log(element, index);
+});
+
+// for clássico - geralmente com iteráveis (array ou strings)
+// for in - retorna o índice ou chave (string, array ou objetos)
+// for of - retorna o valor em si (iteráveis, arrays ou strings)
