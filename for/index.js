52deaaf0cb1c025143759fddd407ce985d915ec1
Glenon Mateus <glenonmateus@gmail.com>
2023-10-08 11:50:39 -0300

    add for in

--- for/index.js
+++ for/index.js
@@ -5,7 +5,23 @@
 //   const par = index % 2 === 0 ? "par" : "ímpar";
 //   console.log(index, par);
 // }
+// const frutas = ["maçã", "pêra", "uva"];
+// for (let index = 0; index < frutas.length; index++) {
+//   console.log(frutas[index]);
+// }
 const frutas = ["maçã", "pêra", "uva"];
-for (let index = 0; index < frutas.length; index++) {
+
+// ler os indices ou chaves do objeto
+for (let index in frutas) {
   console.log(frutas[index]);
+}
+
+const pessoa = {
+  nome: "Glenon",
+  sobronome: "Araujo",
+  idade: 30,
+};
+
+for (const key in pessoa) {
+  console.log(pessoa[key]);
 }
