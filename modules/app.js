bbd042636b25c5ca49728d3d941f985eea5394ed
Glenon Mateus <glenon@predator.localdomain>
2026-05-19 11:48:49 -0300

    feat: add modules

---
+++ modules/app.js
@@ -0,0 +1,11 @@
+import { multiplicacao } from "./mod.js";
+
+console.log(multiplicacao(2, 2));
+
+// const multiplicacao = require("./mod");
+// console.log(multiplicacao(2, 2));
+// console.log(__filename);
+// console.log(__dirname);
+
+// const path = require("path");
+// console.log(path.resolve(__dirname));
---
+++ modules/mod.js
@@ -0,0 +1,16 @@
+// module.exports = function (x, y) {
+//   return x + y;
+// };
+
+// export function multiplicacao(x, y) {
+//   return x * y;
+// }
+
+// export const multiplicacao = (x, y) => {
+//   return x * y;
+// };
+
+const multiplicacao = (x, y) => {
+  return x * y;
+};
+export { multiplicacao };
