735f75f195c5594198ba8631490f26ea4200a3d2
Glenon Mateus <glenonmateus+github@gmail.com>
2026-05-22 07:42:02 -0300

    rafactor: add filename and dirname

--- modules/app.js
+++ modules/app.js
@@ -1,6 +1,13 @@
+import { fileURLToPath } from "url";
+import * as path from "path";
 import { multiplicacao } from "./mod.js";
 
 console.log(multiplicacao(2, 2));
+
+const __filename = path.resolve(fileURLToPath(import.meta.url));
+const __dirname = path.dirname(__filename);
+
+console.log(__filename, __dirname);
 
 // const multiplicacao = require("./mod");
 // console.log(multiplicacao(2, 2));
