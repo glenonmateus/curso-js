e3def98c1cb12bcbf8a97ddb2b50290f5820adca
Glenon Mateus <glenonmateus+github@gmail.com>
2026-06-03 15:11:10 -0300

    feat: add server.js

--- express/server.js
+++ express/server.js
@@ -1,19 +1,45 @@
-import express from "express";
-import route from "./routes.js";
+import "dotenv/config";
 import ejs from "ejs";
+import express from "express";
+import mongoose from "mongoose";
+import route from "./routes.js";
+import { middlewareGlobal } from "./src/middlewares/middleware.js";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import flash from "connect-flash";
+
+mongoose
+  .connect(process.env.DB_CONNECTION_STRING)
+  .then(() => {
+    app.emit("ready!");
+    console.log("Database connected!");
+  })
+  .catch((err) => console.error(err));
+
+const sessionOptions = session({
+  secret: "asçldfajsçldfkjasçlkdfjaçlskjfçlaksjdfçlak",
+  store: MongoStore.create({ client: mongoose.connection.getClient() }),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: process.env.SESSION_MAX_AGE_IN_SECONDS,
+    httpOnly: true,
+  },
+});
 
 const app = express();
-
-// CRUD -> CREATE READ UPDATE DELETE
-// HTTP -> POST   GET  PUT    DELETE
-// /profiles/1?query=value&query=value
-
 app.use(express.urlencoded({ extended: true }));
-app.use(route);
 app.use(express.static("./src/public"));
-
 app.engine(".html", ejs.__express);
 app.set("views", "./src/views");
 app.set("view engine", "html");
+//session
+app.use(sessionOptions);
+app.use(flash());
+//middleware - routes
+app.use(middlewareGlobal);
+app.use(route);
 
-app.listen(3000, () => console.log("Server is running on port 3000"));
+app.on("ready!", () => {
+  app.listen(3000, () => console.log("Server is running on port 3000"));
+});
