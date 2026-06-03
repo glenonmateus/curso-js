bbc08e56bc045321c602deba6622998b73386e35
Glenon Mateus <glenonmateus+github@gmail.com>
2026-06-03 17:15:50 -0300

    feat: add csrf handle error and csrf middleware

--- express/src/middlewares/middleware.js
+++ express/src/middlewares/middleware.js
@@ -3,4 +3,14 @@ const middlewareGlobal = (request, response, next) => {
   next();
 };
 
-export { middlewareGlobal };
+const checkCsrfError = (error, request, response, next) => {
+  if (error && error.code !== "EBADCSRFTOKEN") return next(error);
+  response.status(403).send("Invalid CSRF token");
+};
+
+const csrfMiddleware = (request, response, next) => {
+  response.locals.csrfToken = request.csrfToken();
+  next();
+};
+
+export { middlewareGlobal, checkCsrfError, csrfMiddleware };
